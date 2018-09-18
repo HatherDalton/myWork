57bdcbfdc96ada13612a7dea35e62a5e0b08578f
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-09-18 08:11:09 -0500

    Favorite Movies

---
+++ src/outputExamples/FavoriteMovie.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FavoriteMovie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Disney");
+		System.out.println("Hercules");
+
+	}
+
+}
---
+++ src/outputExamples/HelloWorld.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
