66072d8e70c966383d81dc73581eaa7c445c1f82
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-01 09:11:12 -0500

    MakingChangeOne

---
+++ src/mathOperators/MakeChange.java
@@ -0,0 +1,23 @@
+package mathOperators;
+import java.util.Scanner;
+public class MakeChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+
+		System.out.println("How much did your bill cost?");
+		double cost;
+		cost = input.nextDouble();
+		System.out.println("What did you pay with?");
+		double bill;
+		bill = input.nextDouble();
+		
+		
+		
+		// do this in all change
+		
+		input.close();
+	}
+
+}
