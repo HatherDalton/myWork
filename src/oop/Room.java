60f313a74918703bec41ce1fb03ac0d6e959cd3a
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-11 08:02:14 -0500

    RoomOne

--- src/oop/Room.java
+++ src/oop/Room.java
@@ -1,5 +1,18 @@
 package oop;
-
+//import statements at the top
 public class Room {
-
+	// instance field-data storage
+	// constructor- methods for constructing the object
+	//default constructor
+	public Room()
+	{}
+	
+		//Statement to initialize the state of the object
+		
+	
+	//methods- things the object can do
 }
+
+
+
+//public static void main (String[]args) {
