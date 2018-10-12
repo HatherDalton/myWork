fee3b1acd8aede84417e2fbe5eb2a37e40b0514b
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-11 21:03:32 -0500

    HouseImprovements

---
+++ src/oop/HouseImproveTest.java
@@ -0,0 +1,29 @@
+package oop;
+
+import java.util.Scanner;
+
+public class HouseImproveTest {
+
+	public static void main (String[]args) {
+		HouseImprovement bob = new HouseImprovement();
+		Scanner input = new Scanner(System.in);
+
+		bob.paintWall(200, 200);
+		
+		System.out.println("How many rows are there in your garden?");
+		double rows;
+		rows = input.nextDouble();
+		bob.plantGarden(10);
+		
+		System.out.println("What is the radius of the pool?");
+		double radius = input.nextDouble();
+		System.out.println("What is the height of the pool?");
+		double height = input.nextDouble();
+
+		//double volume= (Math.PI((radius*radius) * height));
+		//bob.fillPool
+
+	}
+	
+	
+}
--- src/oop/HouseImprovement.java
+++ src/oop/HouseImprovement.java
@@ -1,10 +1,28 @@
 package oop;
 
-public class HouseImprovement {
+import java.util.Scanner;
 
+public class HouseImprovement {
+	Scanner input = new Scanner(System.in);
 	public HouseImprovement() {}
 	
-	public void paintWall () {
-		
+	public void paintWall (double length, double width) {
+		double result= length * width;
+		result = result / 200;
+		result= result/ 24;
+		System.out.println("It will take " +result+ "minutes to paint the wall");
+	}
+	public void plantGarden (double length) {
+		double width= 10;
+		double total= length / width;
+		double sum= ((total/15)*2);
+		System.out.println("It will take you "+sum+ " hours to complete the garden");	 
+	}
+	public void fillPool (double gph) {
+		System.out.println("What is the volume of your pool?");
+		double volume;
+		volume = input.nextDouble();
+		double total= volume / 300;
+		System.out.println("It will take you " +total+ "hours to fill your pool.");
 	}
 }
