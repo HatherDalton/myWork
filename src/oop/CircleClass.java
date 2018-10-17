2ba7d2a5796cedb1f18c4f309655be34f24054b9
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-17 09:09:03 -0500

    CircleClass

--- src/oop/CircleClass.java
+++ src/oop/CircleClass.java
@@ -2,4 +2,23 @@ package oop;
 
 public class CircleClass {
 
+	private double radius;
+	private double area;
+	public CircleClass() {
+		radius = 7;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public double getArea() {
+		area = (Math.PI * (radius * radius));
+		return area;
+	}
+	
+	public void setRadius(int newRad) {
+		radius = newRad;
+	}
+	
 }
--- src/oop/CircleClassTest.java
+++ src/oop/CircleClassTest.java
@@ -2,4 +2,12 @@ package oop;
 
 public class CircleClassTest {
 
+	public CircleClassTest()
+	{
+	
+		CircleClass bob = new CircleClass();
+		bob.setRadius(4);
+		System.out.println(bob.getRadius());
+		
+	}
 }
