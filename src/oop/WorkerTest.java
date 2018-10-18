649385caa350297cb23d5df1a4c83a41a6c52c47
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-17 23:09:39 -0500

    Worker

--- src/oop/WorkerTest.java
+++ src/oop/WorkerTest.java
@@ -1,5 +1,12 @@
 package oop;
 
 public class WorkerTest {
-
+	public static void main (String[]args) {
+		Worker bob = new Worker();
+		bob.setHours(8);
+		bob.setRate(10.55);
+		System.out.println(bob.getRate());
+		System.out.println(bob.getHours());
+		System.out.println(bob.payCheck());
+	}
 }
