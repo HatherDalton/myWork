6eed2eed5d31584674431301d14c8254344bd373
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-18 09:05:50 -0500

    CatClass

--- src/oop/CatClass.java
+++ src/oop/CatClass.java
@@ -2,4 +2,34 @@ package oop;
 
 public class CatClass {
 
+	private double age;
+	private double sleep;
+
+	public CatClass() {
+		age = 6;
+		sleep = 4;
+	}
+
+	public CatClass(double a, double slept) {
+		age = a;
+		sleep = slept;
+	}
+	public double getAge() {
+		return age;
+	}
+
+	public double getBirthday() {
+		double birth = age;
+		return birth;
+	}
+
+	public void moreSleep() {
+		sleep= sleep++;
+		
+	}
+
+	public void lessSleep() {
+		sleep= sleep--;
+		
+	}
 }
--- src/oop/CatClassTest.java
+++ src/oop/CatClassTest.java
@@ -2,4 +2,14 @@ package oop;
 
 public class CatClassTest {
 
+	public static void main(String[] args) {
+		CatClass bob = new CatClass();
+		System.out.println(bob.getBirthday());
+		bob.lessSleep();
+		bob.moreSleep();
+		System.out.println("                ");
+	
+		CatClass fred = new CatClass(7,28);
+		System.out.println(fred.getBirthday());
+	}
 }
