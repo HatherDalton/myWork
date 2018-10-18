090388badc896c1fe4f2b07814dac897cf876f4f
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-17 22:58:28 -0500

    Rectangle

--- src/oop/Rectangle.java
+++ src/oop/Rectangle.java
@@ -1,5 +1,39 @@
 package oop;
 
 public class Rectangle {
-
+	private double width;
+	private double length;
+	private double area;
+	private double peri;
+	public Rectangle() {
+		width = 0;
+		length = 0;
+	}
+	
+	public double getLength() {
+		return length;
+	}
+	
+	public double getWidth() {
+		return width;
+	}
+	
+	public double getArea() {
+		area = length * width;
+		return area;
+	}
+	
+	public double getPerimeter() {
+		peri = ((2 * length) + (2 * width));
+		return peri;
+	}
+	
+	public void setLength(int newLen) {
+		length = newLen;
+	}
+	
+	public void setWidth(int newWid) {
+		width = newWid;
+	}
+	
 }
--- src/oop/RectangleTest.java
+++ src/oop/RectangleTest.java
@@ -1,5 +1,14 @@
 package oop;
 
 public class RectangleTest {
+	
+	public static void main (String[]args) {
+	Rectangle bob = new Rectangle();
+	
+	bob.setWidth(4);
+	bob.setLength(5);
 
+	System.out.println(bob.getArea());
+	System.out.println(bob.getPerimeter());
+	}
 }
