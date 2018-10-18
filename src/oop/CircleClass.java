7110f0199f0857bb511fc5330a78c017f45a9a0d
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-18 08:36:46 -0500

    Format

--- src/oop/CircleClass.java
+++ src/oop/CircleClass.java
@@ -5,31 +5,31 @@ public class CircleClass {
 	private double radius;
 	private double area;
 	private double circum;
+
 	public CircleClass() {
 		radius = 7;
 	}
+
 	public CircleClass(double xRadius) {
-		radius= xRadius;
+		radius = xRadius;
 	}
-	
+
 	public double getRadius() {
 		return radius;
 	}
-	
-	
-	
+
 	public double getArea() {
 		area = (Math.PI * (radius * radius));
 		return area;
 	}
-	
+
 	public double getCircum() {
-		circum = (2* (Math.PI * radius));
+		circum = (2 * (Math.PI * radius));
 		return circum;
 	}
-	
+
 	public void setRadius(int newRad) {
 		radius = newRad;
 	}
-	
+
 }
--- src/oop/CircleClassTest.java
+++ src/oop/CircleClassTest.java
@@ -2,18 +2,17 @@ package oop;
 
 public class CircleClassTest {
 
-	public static void main (String[]args) {
-	
-	
+	public static void main(String[] args) {
+
 		CircleClass bob = new CircleClass();
 		bob.setRadius(4);
 		System.out.println(bob.getRadius());
 		System.out.println(bob.getArea());
 		System.out.println(bob.getCircum());
 		System.out.println("                ");
-		
+
 		CircleClass fred = new CircleClass(5);
-		
+
 		System.out.println(fred.getRadius());
 		System.out.println(fred.getArea());
 		System.out.println(fred.getCircum());
