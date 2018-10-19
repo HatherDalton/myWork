d1eec76893d81e114c6bcf2435e30ca2c363035b
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-19 09:11:44 -0500

    Triangle

--- src/oop/Triangle.java
+++ src/oop/Triangle.java
@@ -2,4 +2,34 @@ package oop;
 
 public class Triangle {
 
+	private double base;
+	private double height;
+	private double area;
+	private double peri;
+	private double hypot;
+
+	public Triangle() {
+		base = 9;
+		height = 7;
+	}
+
+	public Triangle(double xBase, double xHeight) {
+		base = xBase;
+		height = xHeight;
+	}
+
+	public double getArea() {
+		area = 0.5 * (base * height);
+		return area;
+	}
+
+	public double getPeri() {
+		peri= base + height + hypot;
+		return peri;
+	}
+	public double getHypot() {
+		hypot= ((base * base) + (height * height));
+		hypot= Math.sqrt(hypot());
+		return hypot;
+	}
 }
