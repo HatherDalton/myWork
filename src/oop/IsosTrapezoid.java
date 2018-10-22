74f1257b4621dfd078fca82d610a6ff043324442
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-10-21 22:32:43 -0500

    TrapezoidOne

--- src/oop/IsosTrapezoid.java
+++ src/oop/IsosTrapezoid.java
@@ -2,4 +2,34 @@ package oop;
 
 public class IsosTrapezoid {
 
+	private double baseOne;
+	private double baseTwo;
+	private double height;
+	private double leg;
+	private double area;
+	private double peri;
+
+	public IsosTrapezoid() {
+		baseOne = 2;
+		baseTwo = 3;
+		height = 4;
+		leg = 5;
+	}
+
+	public IsosTrapezoid(double baseFirst, double baseSecond, double xHeight, double xLeg) {
+		baseOne = baseFirst;
+		baseTwo = baseSecond;
+		height = xHeight;
+		leg = xLeg;
+	}
+	
+	public double getArea() {
+		area= (height*((baseOne + baseTwo)/2)) ;
+		return area;
+	}
+	
+	public double getPeri() {
+		peri= baseOne + baseTwo + (leg*2);
+		return peri;
+	}
 }
