2b6bdf659d0b09085924812ec0903cb7abfe161c
Heather Dalton <19daltoh@elmbrookstudents.org>
2018-11-07 08:31:04 -0600

    WhileTwo11/07/18

--- src/loop/ActivityTest2.java
+++ src/loop/ActivityTest2.java
@@ -8,15 +8,26 @@ public class ActivityTest2 {
 		Scanner input = new Scanner(System.in);
 
 //Activity 5
-		System.out.println("Enter a number");
+		System.out.println("Enter a number (9999 to quit)");
 		int n = input.nextInt();
 		while (n != 9999) {
-			System.out.println(n * 2);
-			System.out.println("Enter a number");
+			System.out.println("Your number times 2 is " + n * 2);
+			System.out.println("Enter a number (9999 to quit)");
 			n = input.nextInt();
 		}
 
 //Activity 8
+		double total =0;
+		int counter=0;
+		int continu  =1;
+		while (continu==1) {
+			System.out.println("Type a number");
+			total+= input.nextDouble();
+			counter++;
+			System.out.println("Continue (1=Yes/0=Now)");
+			continu=input.nextInt();
+		}
+		System.out.println("Average= " + (total/counter));
 
 	}
 }
