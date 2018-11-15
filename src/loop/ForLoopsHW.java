1d7b3891b842fab3b15617dd76bc4af928c5b28a
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-11-14 21:00:48 -0600

    ForLoops

--- src/loop/ForLoopsHW.java
+++ src/loop/ForLoopsHW.java
@@ -8,13 +8,25 @@ public class ForLoopsHW {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 //Sum of Odd
-		int p= 1;
+		int p= 6;
 		int counter=0;
-		for (p=1;p<=1; p=p+2) {
+		for (p=6;p<=1; p=p+2) {
 			counter= counter+p;
 			System.out.println("Sum= "+counter);
 		}
-		
+//Class Average
+		int students;
+		int grade;
+		int sum=0;
+		int average;
+		System.out.println("How many tudents are in the class?");
+		students=input.nextInt();
+		for (students=1; students<=30; students=students-1) {
+			System.out.println("What is the students grade");
+			grade=input.nextInt();
+			sum= sum+grade;
+		}
+		average= sum/students;
 	}
 
 }
