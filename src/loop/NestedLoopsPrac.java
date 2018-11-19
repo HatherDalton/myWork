d0459161f8e8dab22c9391ae0467bc24b6ec9ffd
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-11-19 07:46:11 -0600

    NestedLoopsPrac

---
+++ src/loop/NestedLoopsPrac.java
@@ -0,0 +1,45 @@
+package loop;
+
+import java.util.Scanner;
+
+public class NestedLoopsPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+//DivisorsInput
+		
+//NumberOutput
+		int h=1;
+		for(int i=1;i<=1; i++) {
+			for (int j=1; j<=5;j++) {
+			 if (j==1) {
+				System.out.println(j+"2345"+h);
+			 h++;
+			 }
+			 if (j==2) {
+				 System.out.println(j+"345"+h);
+				 h++;
+			 }
+			 if (j==3) {
+				 System.out.println(j+"45"+h);
+				 h++;
+			 }
+			 if (j==4) {
+				 System.out.println(j+"5"+h);
+				 h++;
+			 }
+			 if (j==5) {
+				 System.out.println(j+""+h);
+				 h++;
+			 }
+			}
+		}
+//PrimeFactorization(Hard)
+//PerfectNumbers
+//Calculator	
+
+//Needed Brackets  System.out.println("" +);
+	}
+}
