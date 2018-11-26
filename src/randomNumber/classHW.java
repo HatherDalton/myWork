215d9a38ffbc6360e17a33ec6822224c457f95d4
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-11-26 09:03:47 -0600

    HW

---
+++ src/randomNumber/classHW.java
@@ -0,0 +1,19 @@
+package randomNumber;
+
+import java.util.Random;
+
+public class classHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//RollDie
+		
+//RollDieVersion
+
+//FlipCoin
+//Yahtzee
+//Yahtzee2
+//Lottery
+	}
+
+}
