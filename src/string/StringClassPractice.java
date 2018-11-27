d3e543d2c71c095231fe284e5c066cd04e805774
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-11-27 09:10:16 -0600

    StringPractice

---
+++ src/string/StringClassPractice.java
@@ -0,0 +1,15 @@
+package string;
+import java.util.Scanner;
+public class StringClassPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input=new Scanner(System.in);
+		System.out.println("What is your age");
+		int age=input.nextInt();
+		System.out.println("What is your Full Name");
+		String name=input.nextLine();
+	}
+
+}
