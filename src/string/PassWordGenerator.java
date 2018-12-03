ebfbdc5ca433cf0d924d8e97d727a8aac81d2faf
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-12-02 19:17:31 -0600

    newPassWordGenerator

--- src/string/PassWordGenerator.java
+++ src/string/PassWordGenerator.java
@@ -8,19 +8,26 @@ public class PassWordGenerator {
 		// TODO Auto-generated method stub
 		Random gen= new Random();
 		Scanner input = new Scanner(System.in);
-		System.out.println("5 letter Word#1");
+		System.out.println("Word#1");
 		String word1= input.nextLine();
-		System.out.println("6 letter Word#2");
+		int i= word1.length();
+		System.out.println("Word#2");
 		String word2= input.nextLine();
-		System.out.println("7 letter Word#3");
-		String word3= input.nextLine();;
-		int length=word2.length();
+		int j= word2.length();
+		int g= word2.length()-1;
+		System.out.println("Word#3");
+		String word3= input.nextLine();
+		int k= word3.length();
 		int min=1;
-		int max=10;
+		int max=9;
+		int num= gen.nextInt(max-min+1);
 		System.out.print("Your new password=  ");
 		System.out.print(word1.substring(0,1));
-		System.out.print(word2.substring(5,6));
-		System.out.print(word3.substring(4,5));
+		System.out.print(word2.substring(g,j));
+		if(num<=k)
+		System.out.print(word3.charAt(num));
+		else
+			System.out.print(word3.substring(1,2));
 		System.out.print(gen.nextInt(max-min+1));
 		System.out.print(gen.nextInt(max-min+1));
 		System.out.print(gen.nextInt(max-min+1));
