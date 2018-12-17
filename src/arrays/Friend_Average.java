dc49bdfaba34f4b8831abc612791fe9318ae0b3e
Hather Dalton <19daltoh@elmbrookstudents.org>
2018-12-17 08:35:17 -0600

    friendAverage

---
+++ src/arrays/Friend_Average.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Friend_Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the number of friends you have");
+		int number=input.nextInt();
+		int[] age= new int[number];
+		String []names = new String[number];
+		for (int i=0; i<=number; i++) {
+			
+		}
+	}
+
+}
