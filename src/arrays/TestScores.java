1fee08428678917fd8f87775088c693f571379bd
HeatherDalton <19daltoh@elmbrookstudents.org>
2018-12-20 08:51:01 -0600

    FinalTestScore

--- src/arrays/TestScores.java
+++ src/arrays/TestScores.java
@@ -14,17 +14,30 @@ public class TestScores {
 		int[][] studtest = new int[stud][test];
 		System.out.println("Test scores= ");
 		for (int row = 0; row < studtest.length; row++) {
-			System.out.println("Student#"+ (1+row));
+			System.out.println("Student#" + (1 + row));
 			for (int column = 0; column < studtest[0].length; column++) {
 				studtest[row][column] = input.nextInt();
 			}
 			System.out.print("");
 		}
-		/**int av;
-		for (int row=0;row< studtest.length; row++) {
-			if(row=1)
+		int av = 0;
+		int num=0;
+		for (int row = 0; row < studtest.length; row++) {
+			for (int column = 0; column < studtest[0].length; column++) {
+				av= av+ studtest[row][column];
+			}
+			System.out.println("Student Average= " + av/studtest[0].length);
+			av=0;
 		}
-		**/
+		int v = 0;
+		for (int column = 0; column < studtest[0].length; column++) {
+			for (int row = 0; row < studtest.length; row++) {
+				v += studtest[row][column];
+			}
+			System.out.println("Test Average= " + (v / studtest.length));
+			v=0;
+		}
+
 		int average = 0;
 		int i = 1;
 		for (int row = 0; row < studtest.length; row++) {
@@ -33,9 +46,8 @@ public class TestScores {
 				i++;
 			}
 		}
-		System.out.println("Test Average= " + (average / i));
-		System.out.println("Student Average= " + (average / i));
-		System.out.println("Class Average= " + (average / i));
+
+		System.out.println("Class Average= " + (average/studtest[0].length));
 
 		//
 	}
