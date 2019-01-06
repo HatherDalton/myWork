da6e2a73e2eb9c78ebd57ec31ed21723e2cd3f8f
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-01-06 16:43:51 -0600

    Activity3part1

--- src/arrays/PostHolidayBreak1.java
+++ src/arrays/PostHolidayBreak1.java
@@ -1,38 +1,31 @@
 package arrays;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class PostHolidayBreak1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner input=new Scanner(System.in);
-		Random gen= new Random();
+		
+		Random generator= new Random();
 		
 		int min= 1;
 		int max= 17;
+		double even=0; //Why does making this a double change things?
 		int [] sph= new int[1340];
-		int num = 0;
-		int students=0;
 		
 		for(int i=0; i<sph.length; i++) {
-			num=min + gen.nextInt(max-min+1);
-			sph[i]= num;
-			num=0;
+			sph[i]= min + generator.nextInt(max-min+1);
 		}
 		
+		
 		for(int i=0; i<sph.length; i++) {
-			if(sph[i]>7)	{
-				
-			}
 			if(sph[i]%2==0) {
-				students++;
+				even++;
 			}
 		}
-		System.out.println("");
-		System.out.println("Households with even amount of students= " + ((students/sph.length)*100));
+		System.out.println("Households with even amount of students= " + ((even/1340)*100)+"%");
 	}
 
 }
--- src/arrays/PostHolidayBreak2.java
+++ src/arrays/PostHolidayBreak2.java
@@ -1,10 +1,23 @@
 package arrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class PostHolidayBreak2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input=new Scanner(System.in);
+		Random gen= new Random();
+		
+		String[] names= new String[687];
+		String firstL= "";
+		
+		System.out.println("Students names (First Last)");
+		for(int i=0; i<=names.length; i++) {
+			
+		}
 	}
 
 }
