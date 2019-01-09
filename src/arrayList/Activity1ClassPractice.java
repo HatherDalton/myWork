0a1c797fda2aab2e3f3bb5809ff2a9c2d2c1b397
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-01-08 20:10:40 -0600

    ArrayListPractice2
    01/08/2019

--- src/arrayList/Activity1ClassPractice.java
+++ src/arrayList/Activity1ClassPractice.java
@@ -9,19 +9,29 @@ public class Activity1ClassPractice {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
-		ArrayList<String>names= new ArrayList<String>();
-		
-		names.add("Bob");
-		names.add("Jane");
-		names.add("Billy");
-		
+		ArrayList<String> names = new ArrayList<String>();
+
+		names.add("Bob Smith");
+		names.add("Jane Miller");
+		names.add(0, "Billy Jones");
+
 		System.out.println("What is your name?");
 		names.add(input.nextLine());
 
-		for(int i=0; i<names.size(); i++) {
+		System.out.println("     ");
+
+		for (int i = 0; i < names.size(); i++) {
 			System.out.println(names.get(i));
 		}
-	
+		System.out.println("     ");
+
+		names.remove(1);
+		// names.remove(names.size()-1);
+
+		System.out.println("Remaining people:");
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println("-->" + names.get(i));
+		}
 ////////////////////////////////////////////////////////////////////
 	}
 }
--- src/arrayList/Activity2_ClassPractice.java
+++ src/arrayList/Activity2_ClassPractice.java
@@ -16,17 +16,30 @@ public class Activity2_ClassPractice {
 		buddy.add(person);
 		Friend person1= new Friend("John Smith", 45);
 		buddy.add(person1);
-		System.out.println("Enter a name");
-		String names= input.nextLine();
-		System.out.println("Enter a age");
-		int xAge= input.nextInt();
-		Friend person2= new Friend(names, xAge);
-		buddy.add(person2);
 		
-		for (int i=0; i<buddy.size(); i++) {
-		System.out.println(buddy.get(i));
+		for (int i=0; i<=1; i++) {
+			System.out.println("Enter a name");
+			String names= input.nextLine();
+			//System.out.println("Enter a age");
+			//int xAge= input.nextInt();
+			Friend person2= new Friend(names, 0);
+			buddy.add(person2);
 		}
 		
+		for (int i=0; i<buddy.size(); i++) {
+		System.out.println(buddy.get(i).getName());
+		}
+		System.out.println("   ");
+		
+		Friend move= buddy.remove(0);
+		buddy.add(3, move);
+		move= buddy.remove(buddy.size());
+		
+		
+		for (int i=0; i<buddy.size(); i++) {
+			System.out.println(buddy.get(i).getName());
+			}
+		
+////////////////////////////////////////////////////////////////////
 	}
-
 }
