e03a1e259d225e681ac311cf3265b2b9146cac4b
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-01-14 08:57:28 -0600

    Notes (01/14/19)

---
+++ src/WrapperClass/ArrayList_for_each_loop.java
@@ -0,0 +1,21 @@
+package WrapperClass;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ArrayList_for_each_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Number of Employees");
+		int num= input.nextInt();
+		ArrayList<Double>workers= new ArrayList <Double>();
+		
+		for (double temp: workers) {
+			System.out.println("xAge?");
+			
+		}
+////////////////////////////////////////////////////////
+	}
+
+}
---
+++ src/WrapperClass/for_each_loopActivity.java
@@ -0,0 +1,22 @@
+package WrapperClass;
+
+import java.util.Scanner;
+
+public class for_each_loopActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Number of students");
+		int num= input.nextInt();
+		int[]ages= new int[num];
+		
+		for (int temp: ages) {
+			System.out.println("xAge?");
+			
+		}
+////////////////////////////////////////////////////////		
+	}
+
+}
