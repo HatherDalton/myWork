8c8630b6de1392418570fa88bb04c38d5974766c
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-01-29 09:05:05 -0600

    HW(01/29/2019)

--- src/designingClass/Book.java
+++ src/designingClass/Book.java
@@ -2,4 +2,24 @@ package designingClass;
 
 public class Book {
 
+	private String title;
+	private int pages;
+	
+	public Book() {
+		title= "NA";
+		pages=0;
+	}
+	
+	public Book (String xTitle, int xPages) {
+		title= xTitle;
+		pages= xPages;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getPages() {
+		return pages;
+	}
 }
---
+++ src/designingClass/Library.java
@@ -0,0 +1,14 @@
+package designingClass;
+
+import java.util.ArrayList;
+
+public class Library {
+	
+	private String name;
+	private ArrayList<Book> books;
+
+	public Library() {
+		name="BEHS Library";
+		//books.add("")
+	}
+}
