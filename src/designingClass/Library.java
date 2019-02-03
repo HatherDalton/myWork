6816b3c1a727620b4e2290b90595b3aff7e0d93b
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-02-03 17:07:32 -0600

    HW(02/03/2019)

--- src/designingClass/Library.java
+++ src/designingClass/Library.java
@@ -3,12 +3,33 @@ package designingClass;
 import java.util.ArrayList;
 
 public class Library {
-	
+
 	private String name;
 	private ArrayList<Book> books;
 
 	public Library() {
-		name="BEHS Library";
-		//books.add("")
+		name = "My Library";
+		for (int i = 0; i < 10; i++) {
+			books.add(new Book());
+		}
 	}
+
+	public Library(String xName, ArrayList<Book> col) {
+		name = xName;
+		books = col;
+	}
+
+	public Book checkOut(int index) {
+		return books.remove(index);
+	}
+
+	public void returnBook(Book temp) {
+		books.add(temp);
+	}
+
+	public void fire() {
+		books.clear();
+	}
+
+///////////////////////////////////////////////////////	
 }
