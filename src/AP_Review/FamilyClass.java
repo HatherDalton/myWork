87af708908a5f3350e8f29309f506465768f2d37
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-03-18 17:37:02 -0500

    #4Attempt2

--- src/AP_Review/FamilyClass.java
+++ src/AP_Review/FamilyClass.java
@@ -1,5 +1,30 @@
 package AP_Review;
 
+import java.util.ArrayList;
+
 public class FamilyClass {
+
+	private ArrayList<Person> adult;
+	private ArrayList<Person> children;
+
+	public FamilyClass(Person n, Person q) {
+		if (n.getAge() < 18) {
+			children.add(n);
+		} else
+			adult.add(n);
+		if (q.getAge() < 18) {
+			children.add(q);
+		} else
+			adult.add(q);
+	}
+
+	public boolean isInFamily(Person m) {
+		boolean answer = false;
+		// if (m.getAge()== adult.getAge() || children.getAge() && m.getName()==
+		// adult.getName() || children.getName()) {
+		// answer= true;
+		// }
+		return answer;
+	}
 
 }
