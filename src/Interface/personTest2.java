607ba4b6e6773e5731088bd4a04c05c49156ce68
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-03-21 08:45:37 -0500

    CastingExamples

--- src/Interface/personTest2.java
+++ src/Interface/personTest2.java
@@ -7,7 +7,7 @@ public class personTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ArrayList<Person> human= new ArrayList<Person>();
+		ArrayList<Person> human = new ArrayList<Person>();
 		human.add(new Student());
 		human.add(new Student());
 		human.add(new Student());
@@ -15,13 +15,34 @@ public class personTest2 {
 		human.add(new Teacher());
 		human.add(new Teacher());
 		human.add(new Teacher());
-		
-		//lots of object manipulation
-		
+
+		// lots of object manipulation
+
+//Get the names of everybody
 		System.out.println("Names of Everyne: ");
-		for (Person folk: human) {
+		for (Person folk : human) {
 			System.out.println(folk.getName());
 		}
-	}
 
+//if the persons name is greater than 10 characters display their age
+		for (Person buddy : human) {
+			if (buddy.getName().length() > 10)
+				System.out.println(buddy.getAge());
+		}
+
+//Make all of the students out sick and display their names
+		for (Person friend : human)
+			if (friend instanceof Student) {
+				Student kid = (Student) friend;
+				kid.outSick();
+				System.out.println(kid.getName());
+			}
+
+//Pay all of the teachers	
+		for (Person adult : human)
+			if (adult instanceof Teacher) {
+				Teacher staff = (Teacher) adult;
+				staff.payMe();
+			}
+	}
 }
