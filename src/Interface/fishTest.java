072c143f5c51ee41f06264f209ed42da5ff6ead5
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-03-25 08:24:07 -0500

    FishStuff(finished)

--- src/Interface/fishTest.java
+++ src/Interface/fishTest.java
@@ -1,30 +1,44 @@
 package Interface;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class fishTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ArrayList<Fish> seafood= new ArrayList<Fish>();
-		seafood.add(new Goldfish(45, 76,"purple"));
-		seafood.add(new Trout(10,34,56));
-		seafood.add(new Goldfish(57, 76,"red"));
-		seafood.add(new Trout(100,3,56));
-		seafood.add(new Goldfish(95, 76,"purple"));
-		seafood.add(new Trout(23,34,56));
-		
-		double longest=0;
-		int num=0;
-
-		for(int i=0; i<seafood.size(); i++) {
-			Fish type=(Fish) seafood;
-			if(type.getLength()>longest) {
-				longest= type.getLength();
-				num=i;
+		ArrayList<Fish> fishes = new ArrayList<Fish>();
+		Random bob = new Random();
+		int max = 1000;
+		int min = 1;
+		for (int i = 0; i < 5; i++) {
+			fishes.add(new Trout(min + bob.nextInt(max - min + 1), min + bob.nextInt(max - min + 1),
+					min + bob.nextInt(max - min + 1)));
+			fishes.add(new Goldfish(min + bob.nextInt(max - min + 1), min + bob.nextInt(max - min + 1), "Orange"));
+		}
+		Fish longest = new Trout(0, 0, 0);
+		Trout oldest = new Trout(0, 0, 0);
+		Goldfish longer = new Goldfish(0, 0, "Blue");
+		for (Fish temp : fishes) {
+			if (temp.getLength() > longest.getLength()) {
+				longest = temp;
+			}
+			if (temp instanceof Trout) {
+				Trout temp2 = (Trout) temp;
+				if (temp2.getAge() > oldest.getAge()) {
+					oldest = temp2;
+				}
+			}
+			if (temp instanceof Goldfish) {
+				Goldfish temp2 = (Goldfish) temp;
+				if (temp2.getLength() > longer.getLength()) {
+					longer = temp2;
+				}
 			}
 		}
-		//seafood.indexof(num).toString();
+		System.out.println(longest.toString());
+		System.out.println(longer.getColor());
+		System.out.println(oldest.getScales());
 	}
 }
