e35dd78132083500dc8c8fa7dcbd6aeff6355224
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-04-10 08:32:58 -0500

    ShapeTest

--- src/inheritance/ShapeTest.java
+++ src/inheritance/ShapeTest.java
@@ -6,12 +6,23 @@ public class ShapeTest {
 		// TODO Auto-generated method stub
 
 		Rectangle box = new Rectangle(4, 7);
-		System.out.println("Area= "+box.getArea());
-		System.out.println("Peremiter= "+box.Perimeter());
-	
-		Trapezoid bob= new Trapezoid(5,6,7,8,10);
-		System.out.println("Area= "+bob.getArea());
-		System.out.println("Peremiter= "+bob.Perimeter());
+		System.out.print("Rectangle:");
+		System.out.println("Area= " + box.getArea());
+		System.out.println("Peremiter= " + box.Perimeter());
+
+		System.out.print("                                ");
+
+		Trapezoid bob = new Trapezoid(5, 6, 7, 8, 10);
+		System.out.print("Trapezoid:");
+		System.out.println("Area= " + bob.getArea());
+		System.out.println("Peremiter= " + bob.Perimeter());
+		
+		System.out.print("                                ");
+
+		Square jeff = new Square (3.9);
+		System.out.print("Square:");
+		System.out.println("Area= " + jeff.getArea());
+		System.out.println("Peremiter= " + jeff.Perimeter());
 	}
 
 }
