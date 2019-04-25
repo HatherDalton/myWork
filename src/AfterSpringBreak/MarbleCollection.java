8add2d430c8e656bcdc53ade36c729fe33d32d0e
HeatherDalton <19daltoh@elmbrookstudents.org>
2019-04-25 08:39:18 -0500

    FRQ#2 revised

--- src/AfterSpringBreak/MarbleCollection.java
+++ src/AfterSpringBreak/MarbleCollection.java
@@ -24,8 +24,8 @@ public class MarbleCollection {
 
 	public int removeColor(String marbleCol) {
 		int x=0;
-		for(int i=0; i< sets.size(); i++) {
-		if (marbleCol== sets.get(i).getColor()) {
+		for(int i=sets.size(); i<0; i--) {
+		if (sets.get(i).getColor().equals(marbleCol)) {
 			x+=sets.get(i).getNum();
 			sets.remove(i);
 			}	
